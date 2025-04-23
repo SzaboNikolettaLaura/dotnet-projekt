f807adc8a662530bebded41dfd766268ce955589
Szabó Nikoletta-Laura <szabo.nikoletta@student.ms.sapientia.ro>
2025-04-23 20:43:12 +0300

    szures

--- obj/Debug/net8.0/DotnetProjekt.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetProjekt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetProjekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d93d7c8997c2442323472994624128c1b2e3b56b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetProjekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetProjekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
