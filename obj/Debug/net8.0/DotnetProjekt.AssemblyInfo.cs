d25a47311f10e7b4dbadee8359b5257d4745c191
Szabó Nikoletta-Laura <szabo.nikoletta@student.ms.sapientia.ro>
2025-04-27 22:58:06 +0300

    idk de ez most mukodik

--- obj/Debug/net8.0/DotnetProjekt.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetProjekt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetProjekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0194f91a084b74d649867e7c3eb468f201a2612")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a17410a479c4def279179d33e8db1e062014c5e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetProjekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetProjekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
