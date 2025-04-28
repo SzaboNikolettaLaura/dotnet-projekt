34f89e609aaf3cafb6346005a8945c87e385d801
Szabó Nikoletta-Laura <szabo.nikoletta@student.ms.sapientia.ro>
2025-04-28 21:37:27 +0300

    kis szepitesek

--- obj/Debug/net8.0/DotnetProjekt.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetProjekt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetProjekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+862bb29e777e11274b0ba8b22ddf18c010d63ede")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+086f0279c60f9d48df872ad75db9a9dd6e068c4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetProjekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetProjekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
